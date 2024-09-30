fa1c3c7edb4af11bf580cf6fe597d7476b3ca8e7
CharlesMich <charleedongo@hotmail.com>
2024-09-30 11:26:51 -0500

    sep 30

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+useEffect(()=> {
+  fetch('/Data/states.json')
+  .then(response => response.json())
+  .then(data => setData(data))
+  .catch(error => console.error("Error fetching data", error))
+}, [])
+
+if (!data) {
+  return <div>Loading...</div>
+}
   return (
    <>
-   <div>Charles</div>
+   <div>{data.states}</div>
    </>
   );
 }
