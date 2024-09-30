386de83600b68999c1d557c5885e20127b4e56cd
CharlesMich <charleedongo@hotmail.com>
2024-09-30 17:20:12 -0500

    added json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react'
+import {State }from './Components/State'
+import {Cuisine} from './Components/Cuisine'
+import {Ingredient} from './Components/Ingredient'
+import {Meal} from './Components/Meal'
+import {VegNonVeg} from './Components/VegNonVeg'
 
 function App() {
+  const [activeTab, setActiveTab] = useState(0);
 
-  const [data, setData] = useState(null);
-
-useEffect(()=> {
-  fetch('/Data/states.json')
-  .then(response => response.json())
-  .then(data => setData(data))
-  .catch(error => console.error("Error fetching data", error))
-}, [])
-
-if (!data) {
-  return <div>Loading...</div>
-}
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+  
   return (
-   <>
-   <div>{data.states}</div>
-   </>
+    <div className=" bg-gray-100">
+      <ul className="tabs flex flex-row gap-10">
+        <div>Sort by</div>
+        <li className={activeTab === 0 ? 'active' : ''} onClick={() => handleTabClick(0)}>
+         State
+        </li>
+        <li className={activeTab === 1 ? 'active' : ''} onClick={() => handleTabClick(1)}>
+          Cuisine
+        </li>
+        <li className={activeTab === 2 ? 'active' : ''} onClick={() => handleTabClick(2)}>
+          Meal
+        </li>
+        <li className={activeTab === 3 ? 'active' : ''} onClick={() => handleTabClick(3)}>
+         Veg / Non Veg
+        </li>
+        <li className={activeTab === 4 ? 'active' : ''} onClick={() => handleTabClick(4)}>
+         Ingredient
+        </li>
+      </ul>
+
+      <div className="content">
+        {activeTab === 0 && <State />}
+        {activeTab === 1 && <Cuisine />}
+        {activeTab === 2 && <Meal/>}
+        {activeTab === 3 && <VegNonVeg/>}
+        {activeTab === 4 && <Ingredient/>}
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/Components/VegNonVeg.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const VegNonVeg = () => {
+  return (
+    <div>This is Veg Non Veg page</div>
+  )
+}
