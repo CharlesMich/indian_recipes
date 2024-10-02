4d56c0cc117efeafd9fa1289cc55ea6a6ee8341c
CharlesMich <charleedongo@hotmail.com>
2024-10-02 14:10:33 -0500

    oct 2

--- frontend/src/Components/Meal.jsx
+++ frontend/src/Components/Meal.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {useEffect, useState} from 'react'
+import meal from '../Constants/meal'
 
 export const Meal = () => {
     const [data, setData] = useState(null);
+
+    console.log(meal)
 
     useEffect(()=> {
       fetch('../Data/meal.json')
@@ -17,14 +20,14 @@ export const Meal = () => {
       return (
        <div className="flex flex-col min-h-screen justify-between bg-gray-100">
         {/* <section className="max-container flex justify-center flex-wrap gap-9 mb-10"> */}
-       <div  className="max-container flex justify-center flex-wrap gap-9 my-10">{data.meal.map((item)=> (
+       <div  className="max-container flex justify-center flex-wrap gap-9 my-10">{meal.map((item)=> (
          <div className="flex sm:w-[300px] 
                   sm:min-w-[200px] w-full rounded-
                   [20px] shadow-3xl px-5 py-5 bg-white">
          <div className="flex flex-row items-center gap-3">
-           {/* <img src={item} alt={label} width={24} height={24}/> */}
+           <img src={item.img} alt={""} width={48} height={24}/>
           
-           <h4 className="mt-5 font-palanquin text-2xl leading-normal font-bold">{item}</h4>
+           <h4 className="mt-5 font-palanquin text-2xl leading-normal font-bold">{item.meal}</h4>
            {/* <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">{item}</p> */}
            </div>
            </div>
---
+++ frontend/src/Constants/meal.js
@@ -0,0 +1,59 @@
+const meal = [
+    {
+        id: 1,
+        img: "../../Images/bread.png",
+        meal: "appetizer"
+    },
+
+    {
+        id: 2,
+        img: "../../Images/bread.png",
+        meal: "bread"
+    },
+    {
+        id: 3,
+        img: "../../Images/idli.png",
+        meal: "breakfast"
+    },
+    {
+        id: 4,
+        img: "../../Images/biryani.png",
+        meal: "biriyani"
+    },
+    {
+        id: 5,
+        img: "",
+        meal: "curry"
+    },
+    {
+        id: 6,
+        img: "../../Images/drink.png",
+        meal: "drink"
+    },
+    {
+        id: 7,
+        img: "",
+        meal: "snack"
+    },
+    {
+        id: 8,
+        img: "../../Images/rice.png",
+        meal: "rice"
+    },
+    {
+        id: 9,
+        img: "../../Images/soup.png",
+        meal: "soup"
+    },
+    {
+        id: 10,
+        img: "../../Images/sweet.png",
+        meal: "sweet"
+    },
+    {
+        id: 11,
+        img: "../../Images/tandoori.png",
+        meal: "tandoori"
+    }
+]
+export default meal
