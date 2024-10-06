2596c71b757ac83d7cff2da23cf81d59330609ef
CharlesMich <charleedongo@hotmail.com>
2024-10-05 21:37:14 -0500

    carousel

---
+++ frontend/src/Components/Carousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Carousel } from "flowbite-react";
+
+export const Carousel1 = () => {
+    return (
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel>
+            <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+              fhsfgsjdhgfjsdf
+            </div>
+            <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+              Slide dkjsfhskdjhfkds
+            </div>
+            <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+              Slide jhkjhrek
+            </div>
+          </Carousel>
+        </div>
+      );
+}
--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { PhotoCard } from './PhotoCard'
+import { Carousel1 } from './Carousel'
 
 
 export const Hero = () => {
   return (
     <div className="flex flex-col min-h-screen justify-between  bg-gray-100 max-container">
 
-   
+   <Carousel1/>
     <div className='max-container flex flex-row flex-wrap gap-5 justify-center mt-10'>
     <PhotoCard img="../../Images/state_hero_img.jpg" title={"Browse by States"}/>
    <PhotoCard img="../../Images/state_hero_img.jpg" title={"Browse by Cuisine"}/>
