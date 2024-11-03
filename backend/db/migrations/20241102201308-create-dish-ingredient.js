f9deb4bc490af9eba4bb3b97644a2328a36ce5a9
CharlesMich <charleedongo@hotmail.com>
2024-11-02 23:01:18 -0500

    nov 2

--- backend/db/migrations/20241102201308-create-dish-ingredient.js
+++ backend/db/migrations/20241102201308-create-dish-ingredient.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 
 /** @type {import('sequelize-cli').Migration} */
 
@@ -41,9 +46,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Dishingredients');
+    options.tablename = "Dishingredients"
+    await queryInterface.dropTable(options);
   }
 };
